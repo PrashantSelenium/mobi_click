edf63d3eb49fb3f30f5c1d73c62aea9dd135c4f6
Sreepad Bhagwat <bhsree@yahoo.com>
2015-05-10 22:28:00 -0400

    removed put statements

--- lib/mobi_click/accessors.rb
+++ lib/mobi_click/accessors.rb
@@ -3,18 +3,13 @@ module MobiClick
 
 
     def text(name)
-      puts "ssssssssssss #{name}"
-      #name=name.to_s
+
       define_method("#{name}") do
-        puts "inside Accessors::text #{MobiClick.platform}"
-        puts MobiClick.platform.driver
         puts "Actual : #{MobiClick.platform.driver.find_element(:name,'UICatalog').text()} -- Expected : 'UICatalog'"
-        puts "inside method name"
       end
-      puts 'end'
 
       define_method("#{name}?") do
-        puts 'question'
+        puts 'test name?'
       end
 
     end
--- lib/mobi_click/adapter.rb
+++ lib/mobi_click/adapter.rb
@@ -4,20 +4,6 @@ module MobiClick
   module Adapter
     @@tools ||={}
 
-
-=begin
-    def self.set_tool(tool)
-    @a="Appium"
-
-    end
-
-    def self.get_tool
-      puts Object.const_get("MobiClick::#{@a}")
-      Object.const_get("MobiClick::#{@a}")
-      end
-
-=end
-
     def self.get
       @@tools
     end
@@ -29,4 +15,3 @@ module MobiClick
   end
 end
 
-require "mobi_click/platforms/appiuml"
