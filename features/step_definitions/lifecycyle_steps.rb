1758bae18a32a49644179a248f46efd74f9bcc00
Sreepad Bhagwat <bhsree@yahoo.com>
2015-05-10 22:26:26 -0400

    added reference to screen object class

--- features/step_definitions/lifecycyle_steps.rb
+++ features/step_definitions/lifecycyle_steps.rb
@@ -1,10 +1,11 @@
+
+
+
 When(/^I start the server$/) do
   #pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then(/^the application should be running\.$/) do
-  #pending # Write code here that turns the phrase above into concrete actions
-  puts "Verifying text on the catalog page"
-  puts "Actual : #{$driver.find_element(:name,'UICatalog').text()} -- Expected : 'UICatalog'"
- fail unless $driver.find_element(:name,'UICatalog').text().eql? 'UICatalog'
+  ms=MainScreen.new
+  ms.get_UICatalog_text
 end
